5777218adf91e64637f287a19ada74805f5c4984
Jacob Arychuk <156041293+jacobarychuk@users.noreply.github.com>
2025-06-14 19:37:57 -0700

    feat: SQLite database schema (#2)

---
+++ init_db.py
@@ -0,0 +1,14 @@
+import sqlite3
+
+with sqlite3.connect("sensor_data.db", isolation_level=None) as con:
+    cur = con.cursor() # Used to execute SQL statements and fetch results from SQL queries
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS samples (
+            timestamp INTEGER PRIMARY KEY,
+            glycol REAL NOT NULL,
+            preheat REAL NOT NULL,
+            ambient REAL NOT NULL,
+            source REAL NOT NULL,
+            hot REAL NOT NULL
+        ) STRICT
+    """)
