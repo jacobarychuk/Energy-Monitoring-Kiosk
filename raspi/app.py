22c22028804492f04de1c606d266d30df7bce5cb
Jacob Arychuk <156041293+jacobarychuk@users.noreply.github.com>
2025-06-15 22:59:13 -0700

    feat: remove temperature sensor support from Raspberry Pi

--- raspi/app.py
+++ raspi/app.py
@@ -1,15 +1,7 @@
 from flask import Flask, render_template
-from w1thermsensor import W1ThermSensor
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    sensor_data = []
-    for sensor in W1ThermSensor.get_available_sensors():
-        sensor_data.append({
-            "id": sensor.id,
-            "temperature": sensor.get_temperature()
-        })
-
-    return render_template("index.html", sensors=sensor_data)
+    return render_template("index.html")
