b66adaabdf020069553f5eadbad5e95397273f7a
Jacob Arychuk <156041293+jacobarychuk@users.noreply.github.com>
2025-06-16 16:01:01 -0700

    feat: establish Wi-Fi and HTTP link between Raspberry Pi and ESP32 (#4)

--- esp32/src/main.cpp
+++ esp32/src/main.cpp
@@ -1,6 +1,9 @@
 #include <Arduino.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include "secrets.h"
 
 #define NUM_SENSORS 5
 DeviceAddress sensorAddresses[NUM_SENSORS] = {
@@ -9,6 +12,13 @@ DeviceAddress sensorAddresses[NUM_SENSORS] = {
   { 0x28, 0xF9, 0xAF, 0x51, 0x00, 0x00, 0x00, 0xF1 }, // Ambient
   { 0x28, 0x25, 0x8C, 0x50, 0x00, 0x00, 0x00, 0x71 }, // Source
   { 0x28, 0xC7, 0xC0, 0x50, 0x00, 0x00, 0x00, 0xFE }  // Hot
+};
+const String sensorNames[NUM_SENSORS] = {
+  "glycol",
+  "preheat",
+  "ambient",
+  "source",
+  "hot"
 };
 
 // Data wire is connected to GPIO 15
@@ -20,6 +30,15 @@ DallasTemperature sensors(&oneWire);
 void setup() {
   Serial.begin(115200);
   sensors.begin();
+
+  WiFi.begin(SSID, PASSWORD);
+  Serial.print("Connecting to Wi-Fi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.print("\nConnected! IP Address: ");
+  Serial.println(WiFi.localIP());
 }
 
 void loop() {
@@ -27,14 +46,25 @@ void loop() {
 
   delay(750);
 
+  String json = "{";
+
   for (int i = 0; i < NUM_SENSORS; i++) {
     float tempC = sensors.getTempC(sensorAddresses[i]);
-    Serial.print("Temperature Sensor ");
-    Serial.print(i);
-    Serial.print(": ");
-    Serial.print(tempC);
-    Serial.println("°C");
+
+    json += "\"" + sensorNames[i] + "\": " + String(tempC);
+
+    if (i < NUM_SENSORS - 1) {
+      json += ",";
+    }
   }
+
+  json += "}";
+
+  HTTPClient http;
+  http.begin("http://raspberrypi.local:5000/data");
+  http.addHeader("Content-Type", "application/json");
+  http.POST(json);
+  http.end();
 
   delay(5000);
 }
