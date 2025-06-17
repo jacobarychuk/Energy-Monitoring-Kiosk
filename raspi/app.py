36df98bd7c719c35ae6e4a91d000b847a8a2127e
Jacob Arychuk <156041293+jacobarychuk@users.noreply.github.com>
2025-06-17 12:23:31 -0700

    feat(raspi): update /data endpoint to insert into samples table

--- raspi/app.py
+++ raspi/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import sqlite3
 
 app = Flask(__name__)
 
@@ -12,4 +13,25 @@ def index():
 def receive_data():
     content = request.get_json()
     print("Received:", content)
+
+    # Extract expected fields
+    expected_fields = ["timestamp", "glycol", "preheat", "ambient", "source", "hot"]
+    if not all(key in content for key in expected_fields):
+        return 400
+
+    # Insert into database
+    with sqlite3.connect("sensor_data.db", isolation_level=None) as con:
+        cur = con.cursor() # Used to execute SQL statements and fetch results from SQL queries
+        cur.execute("""
+            INSERT INTO samples (timestamp, glycol, preheat, ambient, source, hot)
+            VALUES (?, ?, ?, ?, ?, ?)
+        """, (
+            content["timestamp"],
+            content["glycol"],
+            content["preheat"],
+            content["ambient"],
+            content["source"],
+            content["hot"]
+        ))
+
     return "OK"
